085cce11dcf5c46d652976f678f06c85f4022b70
Gerwin <gerwinvillalino14@gmail.com>
2024-12-06 23:21:59 +0800

    first commit

---
+++ models/taskModel.js
@@ -0,0 +1,8 @@
+// models/taskModel.js
+let tasks = [
+    { id: 1, task: "Learn HTTP Methods", status: "Incomplete" },
+    { id: 2, task: "Organize Project Structure", status: "Complete" },
+  ];
+  
+  module.exports = tasks;
+  
